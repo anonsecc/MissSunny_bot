1742b1be9b640a9823cd93964a108105c24df6c0
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-05-10 10:09:53 +0530

    Update memes.py

--- IHbot/modules/memes.py
+++ IHbot/modules/memes.py
@@ -4,6 +4,7 @@ from spongemock import spongemock
 from typing import Optional, List
 from telegram import Message, Update, Bot, User
 from pathlib import Path
+from zalgo_text import zalgo
 import glob
 import os
 
