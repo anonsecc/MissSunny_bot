6cfbae851fd2fb6c377775dbb363e00516626a17
misssunnybot <50962338+misssunnybot@users.noreply.github.com>
2019-05-24 00:10:22 +0530

    Update global_bans.py

--- IHbot/modules/global_bans.py
+++ IHbot/modules/global_bans.py
@@ -62,7 +62,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
         return
 
     if user_id == bot.id:
-        message.reply_text("-_- So funny, lets gban myself why don't I? Nice try.")
+        message.reply_text("-,- So funny, lets gban myself why don't I? Nice try.")
         return
 
     try:
@@ -132,7 +132,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                    "{} has been successfully gbanned!".format(mention_html(user_chat.id, user_chat.first_name)),
                    html=True)
-    message.reply_text("Person has been \"Dealt with\".")
+    message.reply_text("That person is now crying in the Corner!")
 
 @run_async
 def ungban(bot: Bot, update: Update, args: List[str]):
